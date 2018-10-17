d11ee8c3ba27e15135e3d6b542eb06b9f7732ae0
lucjadolega1 <43589245+lucjadolega1@users.noreply.github.com>
2018-10-16 18:51:48 -0700

    Add files via upload

---
+++ Javatask.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicswitch;
+
+/**
+ *
+ * @author konra
+ */
+public class Javatask {
+    public static void main(String[] args){
+        final int CHOICE = 123;
+        switch(CHOICE){
+            case 1: 
+                System.out.println("You made your first choice");
+                break;
+            case 2: 
+                System.out.println("You made your second choice");
+                break;
+            case 10:
+                System.out.println("Cases can match all the values");
+                break;
+            default: 
+                System.out.println("Default case");
+        } // close switching block
+    } // close main method
+} // close class
