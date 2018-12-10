adea31de6b8b8ee7ff7df07613e59fe3b9636583
lucjadolega1 <43589245+lucjadolega1@users.noreply.github.com>
2018-12-10 11:02:24 -0500

    Add files via upload

---
+++ DoTasks.java
@@ -0,0 +1,39 @@
+/*
+
+ * This is class called DoTasks
+
+ * this class contains main method
+
+
+ */
+package assistant;
+
+/**
+ *
+ *
+ *
+ * @author lucja
+ *
+ */
+public class DoTasks {
+
+    //create main method
+    public static void main(String[] args) {
+
+        // create objects
+        PersonalAssistant p = new PersonalAssistant();
+
+        PersonalAssistant r = new PersonalAssistant();
+
+        PersonalAssistant o = new PersonalAssistant();
+
+        PersonalAssistant y = new PersonalAssistant();
+
+        p.canIBuyIt();
+
+        y.checkDiscount();
+
+        o.priceWithTax();
+
+    } // close main method
+} // close class
